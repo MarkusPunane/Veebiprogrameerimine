fe1eca3eb1251c3747ae17a82b6af15699c79d8b
MarkusPunane <MarkusPunane@users.noreply.github.com>
2024-09-30 11:52:27 +0300

    EJS view engine kasutusele võetud

--- index.js
+++ index.js
@@ -1,8 +1,36 @@
 const express = require("express");
+const dateTime = require("./dateTime");
+
+
+
 const app = express();
 
+//määran view mootori
+app.set("view engine", "ejs");
+//määran avalike, jagatavate failide kausta 
+app.use(express.static("public"));
+
 app.get("/", (req, res)=>{
-	res.send("Express läks käima.");
+	//res.send("Express läks käima.");
+	res.render("index");
 });
 
+app.get("/timenow", (req, res)=>{
+	const weekdayNow = dateTime.weekDayEt();
+	const dateNow = dateTime.dateFormattedEt();
+	const timeNow = dateTime.timeFormattedEt();
+	res.render("timenow", {nowWD: weekdayNow, nowD: dateNow, nowT: timeNow});
+});
+
+app.get("/vanasonad", (req, res)=>{
+	let folkWisdom = [];
+	fs.readFile("public/textfiles/vanasonad.txt", "utf8", (err, data)=>{});
+		if(err){
+			throw err;
+		}
+		else {
+			folkWisdom = data.split(";");
+			res.render = ("justlist", {h2: "Vanasõnad", listData: folkWisdom});
+		}	
+	});	
 app.listen(5202);
